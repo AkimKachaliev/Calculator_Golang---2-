265c039042c5f7efe773fd4bcabdb864233b81f9
AkimKachaliev <118452338+AkimKachaliev@users.noreply.github.com>
2024-04-21 19:45:32 +0300

    Add files via upload

---
+++ mod.go
@@ -0,0 +1,9 @@
+module github.com/AkimKachaliev/Calculator_Golang---2-
+
+go 1.18
+
+require (
+github.com/gorilla/context v1.1.1
+github.com/gorilla/mux v1.8.0
+github.com/dgrijalva/jwt-go v3.2.0
+)
