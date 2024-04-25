a454f261a7c2c5d306bfe5ffd5fdbd1c8db6959f
AkimKachaliev <118452338+AkimKachaliev@users.noreply.github.com>
2024-04-25 09:25:05 +0300

    Add files via upload

--- mod.go
+++ mod.go
@@ -1,9 +1,10 @@
-module github.com/AkimKachaliev/Calculator_Golang---2-
+module github.com/yourusername/distributed-calculator
 
 go 1.18
 
 require (
-github.com/gorilla/context v1.1.1
+github.com/google.golang.org/grpc v1.63.2
 github.com/gorilla/mux v1.8.0
 github.com/dgrijalva/jwt-go v3.2.0
+github.com/mattn/go-sqlite3 1.14.16
 )
---
+++ sum.go
@@ -0,0 +1,8 @@
+github.com/dgrijalva/jwt-go v0.10.0 h1:TKf1hXg9/+s3pB55Gm+vS5rZCb8/JYRpJkB3i64q6/Q=
+github.com/dgrijalva/jwt-go v0.10.0/go.mod h1:RjK+Q+/Kz9JL34/Kz/3c+8L1c+L5c+LpKaKc+LpKaKo=
+github.com/gorilla/mux v1.8.0 h1:rJYz7/WB6/+zT/qzT/qzT/qzT/qzT/qzT/qzT/qzT/qzT/qzT/q=
+github.com/gorilla/mux v1.8.0/go.mod h1:RjK+Q+/Kz9JL34/Kz/3c+8L1c+L5c+LpKaKc+LpKaKo=
+github.com/mattn/go-sqlite3 v1.14.8 h1:4+JjUbFq8yvP/+qzT/qzT/qzT/qzT/qzT/qzT/qzT/qzT/qzT/q=
+github.com/mattn/go-sqlite3 v1.14.8/go.mod h1:RjK+Q+/Kz9JL34/Kz/3c+8L1c+L5c+LpKaKc+LpKaKo=
+github.com/yourusername/distributed-calculator v0.0.0-00010101000000-000000000000 h1:4+JjUbFq8yvP/+qzT/qzT/qzT/qzT/qzT/qzT/qzT/qzT/qzT/q=
+github.com/yourusername/distributed-calculator v0.0.0-00010101000000-000000000000/go.mod h1:RjK+Q+/Kz9JL34/Kz/3c+8L1c+L5c+LpKaKc+LpKaKo=
