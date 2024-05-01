b063a6b24bc6736af786a887b65a63a4f7de3c10
AkimKachaliev <118452338+AkimKachaliev@users.noreply.github.com>
2024-05-01 21:55:13 +0300

    Update mod.go

--- mod.go
+++ mod.go
@@ -1,10 +1,9 @@
-module github.com/yourusername/distributed-calculator
+module github.com/AkimKachaliev/Calculator_Golang---2-
 
-go 1.18
+go 1.21.0
 
 require (
-github.com/google.golang.org/grpc v1.63.2
-github.com/gorilla/mux v1.8.0
-github.com/dgrijalva/jwt-go v3.2.0
-github.com/mattn/go-sqlite3 1.14.16
+	github.com/Knetic/govaluate v3.0.0+incompatible
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/mattn/go-sqlite3 v1.14.22
 )
