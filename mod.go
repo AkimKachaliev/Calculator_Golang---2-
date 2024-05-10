fd9457de6640601d44505f3ebab0681a47752a32
AkimKachaliev <118452338+AkimKachaliev@users.noreply.github.com>
2024-05-10 15:02:57 +0300

    Delete mod.go

--- mod.go
+++
@@ -1,9 +0,0 @@
-module github.com/AkimKachaliev/Calculator_Golang---2-
-
-go 1.21.0
-
-require (
-	github.com/Knetic/govaluate v3.0.0+incompatible
-	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/mattn/go-sqlite3 v1.14.22
-)
